52b22185f4c91c7a267c6a8f8271e0a0264a2c1a
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-07-12 20:44:12 +0100

    Finished first model

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,28 +1,31 @@
 class FlatsController < ApplicationController
-
+  before_action :find_flat, except: [:index, :new, :create]
 
   def index
     @flats = Flat.all
   end
 
   def show
-
   end
 
-
   def new
-
+    @flat = Flat.new
   end
 
   def create
-
+    @flat = Flat.new(flats_params)
+    @flat.save
+    redirect_to flats_path
   end
-
+  
   def edit
-
   end
-
+  
   def update
+    @flat = Flat.new
+    @flat.update(flats_params)
+    redirect_to flats_path
+  end
 
   private
 
@@ -33,18 +36,5 @@ class FlatsController < ApplicationController
   def find_flat
     @flat = Flat.find(params[:id])
   end
-
-end
-
-
-
-
-
-
-
-
-
-
-
 
 end
